d94d84535de7d610a887b5ad38cb2f9d58fe4c48
saulo filho <hello@saulofilho.com>
2021-08-31 17:42:38 -0300

    [feat] contact page

--- core/views.py
+++ core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse('Hello world')
+  return render(request, 'home.html')
+
+def contact(request):
+  return render(request, 'contact.html')
